888997a8320dd85b96ca58e2e7844036ca3bc13a
Alvagit25 <alvarodesegovia@gmail.com>
2022-09-28 10:54:51 +0200

    validación registro usuario

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -20,7 +20,20 @@ const registrar = async (req, res) => {
     .notEmpty()
     .withMessage("El nombre no puede ir vacio")
     .run(req);
+  await check("email")
+    .isEmail()
+    .withMessage("El email no es correcto")
+    .run(req);
+  await check("password")
+    .isLength({ min: 6 })
+    .withMessage("El password debe tener al menos 6 caracteres")
+    .run(req);
+  await check("repetir-password")
+    .equals("password")
+    .withMessage("Los passwords no son iguales")
+    .run(req);
   let resultado = validationResult(req);
+  // Verificar que el resultado este vacio
   res.json(resultado.array());
   // importar modelo usuario
   const usuario = await Usuario.create(req.body);
